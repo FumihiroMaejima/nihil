223bacfd96f4c45676a23a64e5008d36eae15446
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-01-04 22:45:57 +0900

    add seeder, migration, model files,

--- app/backend/database/seeders/DatabaseSeeder.php
+++ app/backend/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,22 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         // $this->call(AdminsLogTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(RolePermissionsTableSeeder::class);
+        $this->call(AdminsRolesTableSeeder::class);
+        $this->call(GameAbilityTableSeeder::class);
+        $this->call(GameAreaTableSeeder::class);
+        $this->call(GameDefenseEquipmentTableSeeder::class);
+        $this->call(GameOffenceEquipmentTableSeeder::class);
+        $this->call(GameEnemiesTableSeeder::class);
+        $this->call(GameCharacterTableSeeder::class);
+        $this->call(GameEventTableSeeder::class);
+        $this->call(GameItemTableSeeder::class);
+        $this->call(GameTitleTableSeeder::class);
+        $this->call(GamePlayerTableSeeder::class);
+        $this->call(GamePlayerLogTableSeeder::class);
+        $this->call(GamePlayerItemListTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
