6df5f2406cc602e55ef97e35dae7d4a5e490255f
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-01-18 02:17:25 +0900

    add sample pages.

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // pages
 import { Home } from '@/pages/Home'
+// sample pages
+// import { Graph } from '@/pages/sample/Graph'
+import { Sample } from '@/pages/sample/Sample'
+import { Picsum } from '@/pages/sample/Picsum'
+import { Test1 } from '@/pages/sample/Test1'
 
 export const AppRouter = (): JSX.Element => {
   // process.envがdevelopかの判定
@@ -15,6 +20,9 @@ export const AppRouter = (): JSX.Element => {
     <BrowserRouter basename={servicePathName}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sample" element={<Sample />} />
+        {isDevelop && <Route path="/picsum" element={<Picsum />} />}
+        {isDevelop && <Route path="/test1" element={<Test1 />} />}
       </Routes>
     </BrowserRouter>
   )
