35d2a9f57df0af485b6ca3d3fd7215fd3a730bc8
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-01-24 01:16:23 +0900

    add part text chip box.

---
+++ frontend/src/components/parts/form/PartsTextChipBox.tsx
@@ -0,0 +1,65 @@
+import React, {
+  FormEventHandler,
+  FocusEventHandler,
+  MouseEventHandler,
+} from 'react'
+import { PartsSimpleChip } from '@/components/parts/chip/PartsSimpleChip'
+
+type Props = {
+  value: string
+  className?: string
+  selectedValue?: string
+  onInput?: FormEventHandler<HTMLInputElement>
+  onFocus?: FocusEventHandler<HTMLInputElement>
+  onBlur?: FocusEventHandler<HTMLInputElement>
+  onClickClose?: MouseEventHandler<HTMLButtonElement>
+  placeholder?: string
+  maxLength?: number
+  required?: boolean
+  disabled?: boolean
+  readOnly?: boolean
+}
+
+export const PartsTextChipBox: React.VFC<Props> = ({
+  value = '',
+  className = undefined,
+  selectedValue = undefined,
+  onInput = undefined,
+  onFocus = undefined,
+  onBlur = undefined,
+  onClickClose = undefined,
+  placeholder = undefined,
+  maxLength = undefined,
+  required = undefined,
+  disabled = false,
+  readOnly = false,
+}) => {
+  return (
+    <div className={`parts-text-chip-box${className ? ' ' + className : ''}`}>
+      {selectedValue && (
+        <div className={`parts-text-chip-box__selected-area`}>
+          <PartsSimpleChip
+            label={selectedValue}
+            isClose={true}
+            onClickClose={onClickClose}
+          />
+        </div>
+      )}
+      <input
+        // className={`parts-simple-text-field`}
+        type="text"
+        value={value}
+        onInput={onInput}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        required={required}
+        disabled={disabled}
+        readOnly={readOnly}
+      />
+    </div>
+  )
+}
+
+export default PartsTextChipBox
