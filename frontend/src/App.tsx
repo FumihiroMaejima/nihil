c5382aba3a4c2bda7f0e5079ee7b12c83d8d400d
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-03 23:20:07 +0900

    add AuthAppProviderContainer to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,23 @@ import { GlobalFooter } from '@/components/_global/GlobalFooter'
 import { GlobalHeader } from '@/components/_global/GlobalHeader'
 import { GlobalBackToPageTopButton } from '@/components/_global/GlobalBackToPageTopButton'
 import { NotificationProviderContainer } from '@/components/container/NotificationProviderContainer'
+import { AuthAppProviderContainer } from '@/components/container/AuthAppProviderContainer'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className="app">
-      <NotificationProviderContainer>
-        <GlobalBackToPageTopButton />
-        <GlobalHeader />
-        <div className="app-content">
-          <AppRouter />
-        </div>
-        <GlobalFooter />
-      </NotificationProviderContainer>
+      <AuthAppProviderContainer>
+        <NotificationProviderContainer>
+          <GlobalBackToPageTopButton />
+          <GlobalHeader />
+          <div className="app-content">
+            <AppRouter />
+          </div>
+          <GlobalFooter />
+        </NotificationProviderContainer>
+      </AuthAppProviderContainer>
     </div>
   )
 }
