5a534fd9b63370aa7eddc9006776cbf5587a3586
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-04 02:06:15 +0900

    add text-align setting.

--- frontend/src/pages/sample/Login.tsx
+++ frontend/src/pages/sample/Login.tsx
@@ -30,11 +30,12 @@ export const Login: React.VFC = () => {
       <div className="mxy-4">
         <div className="util-border-full-solid-2p__color--dark-grey util-border-radius__round--5p util-color__text--dark-grey">
           <div className="mxy-4 d-flex flex-align-center">
-            <label className="mr-2" htmlFor="email">
-              email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label className="width-2 text-left" htmlFor="email">
+              email
             </label>
             <PartsSimpleTextField
               id="email"
+              className="width-8"
               value={emailValue}
               onInput={(e) => setEmailValue(e.currentTarget.value)}
               placeholder="email"
@@ -43,11 +44,12 @@ export const Login: React.VFC = () => {
           </div>
 
           <div className="mxy-4 d-flex flex-align-center">
-            <label className="mr-2" htmlFor="password">
+            <label className="width-2 text-left" htmlFor="password">
               password
             </label>
             <PartsSimpleTextField
               id="password"
+              className="width-8"
               value={passwordValue}
               onInput={(e) => setPasswordValue(e.currentTarget.value)}
               placeholder="password"
