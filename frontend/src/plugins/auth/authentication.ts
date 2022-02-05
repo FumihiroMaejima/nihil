36a80d4e030f811c0a4e7f5bdaaf4a2400367391
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-05 14:38:26 +0900

    remove unused plugins.

--- frontend/src/plugins/auth/authentication.ts
+++ frontend/src/plugins/auth/authentication.ts
@@ -27,12 +27,16 @@ export default class Authentication {
   async loginRequest(data: { email: string; password: string }) {
     axios.defaults.withCredentials = true
     return await axios
-      .post(this.options.authLogin, data, {})
+      .post(this.options.authLogin, data, {
+        timeout: 5000,
+        timeoutErrorMessage:
+          "login request is time out. it's not sever response ",
+      })
       .then((response: AxiosResponse<any>) => {
         return { data: response.data, status: response.status }
       })
       .catch((error: AxiosError<any>) => {
-        // for check console.error('axios error' + JSON.stringify(error.message, null, 2))
+        // console.error('axios error' + JSON.stringify(error.message, null, 2))
         return {
           data: error,
           status: error.response ? error.response.status : 500,
