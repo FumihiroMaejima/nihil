8f295d020927d1e3fc06509524731b4b2abcfbec
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-05 16:42:08 +0900

    create GlobalContextWrapper.

---
+++ frontend/src/components/_global/GlobalContextWrapper.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import { PartsSimpleToast } from '@/components/parts/toast/PartsSimpleToast'
+import { PartsCircleLoading } from '@/components/parts/PartsCircleLoading'
+// global context
+// import { AuthAppContext } from '@/components/container/AuthAppProviderContainer'
+import { ToastContext } from '@/components/container/ToastProviderContainer'
+import { GlobalLoadingContext } from '@/components/container/GlobalLoadingProviderContainer'
+
+export const GlobalContextWrapper: React.VFC = () => {
+  const { isOpenLoading } = useContext(GlobalLoadingContext)
+  const { state, updateToastState } = useContext(ToastContext)
+  // const { login } = useContext(AuthAppContext)
+  console.log(
+    'global wrapper component.: ' + JSON.stringify(updateToastState, null, 2)
+  )
+
+  return (
+    <div>
+      {isOpenLoading && <PartsCircleLoading />}
+      <PartsSimpleToast
+        value={state.isDisplay}
+        data={{ message: state.message, status: state.status }}
+        onAnimationEnd={() => {
+          updateToastState('close', 'normal', false)
+        }}
+      />
+    </div>
+  )
+}
+
+export default GlobalContextWrapper
