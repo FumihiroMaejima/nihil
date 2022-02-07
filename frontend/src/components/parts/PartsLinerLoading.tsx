25a8a255d5d536aa54a1026f62277f16261c90f9
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-08 02:22:45 +0900

    add liner loading component.

---
+++ frontend/src/components/parts/PartsLinerLoading.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/* type Props = {
+  open: boolean
+} */
+
+export const PartsLinerLoading: React.VFC = () => {
+  return (
+    <div className="parts-liner-loading">
+      <div className="parts-liner-loading__line"></div>
+      <div className="parts-liner-loading__line"></div>
+      <div className="parts-liner-loading__line"></div>
+    </div>
+  )
+}
+
+export default PartsLinerLoading
