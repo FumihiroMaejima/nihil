84d732a77c724ab796fac0129a413ddf6adba862
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-19 16:55:37 +0900

    modify next.config basepath and so on.

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,39 @@
+// next.js/packages/next/shared/lib/constants.ts
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// default setting
+/* const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig */
+
+// custom setting
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    poweredByHeader: false,
+    basePath: '/admin',
+    distDir: 'dist',
+  }
+  return nextConfig
+
+  // if Divide config in environmental.
+  /* if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      // TODO development only config options here
+    }
+  }
+
+  return {
+    // TODO config options for all phases except development here
+    // nextConfig,
+  } */
+}
