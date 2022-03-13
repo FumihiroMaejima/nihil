e5b13fb5d222273f973acdc4c6f3d41a14f1a9fe
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-03-13 23:39:25 +0900

    add data count by env method.

--- app/backend/database/seeders/AdminsLogTableSeeder.php
+++ app/backend/database/seeders/AdminsLogTableSeeder.php
@@ -10,6 +10,7 @@ class AdminsLogTableSeeder extends Seeder
 {
     private const TABLE_NAME = 'admins_log';
     private const SEEDER_DATA_LENGTH = 5;
+    private const SEEDER_DEVELOP_DATA_LENGTH = 100;
     private int $count = 5;
 
     /**
@@ -31,6 +32,9 @@ class AdminsLogTableSeeder extends Seeder
         // insert用データ
         $data = [];
 
+        // データ数
+        $this->count = $this->getSeederDataLengthByEnv(Config::get('app.env'));
+
         // 1~$this->countの数字の配列でforを回す
         foreach (range(1, $this->count) as $i) {
             $row = $template;
@@ -44,5 +48,25 @@ class AdminsLogTableSeeder extends Seeder
 
         // テーブルへの格納
         DB::table(self::TABLE_NAME)->insert($data);
+    }
+
+    /**
+     * get data length by env.
+     * @param string $envName
+     *
+     * @return int
+     */
+    private function getSeederDataLengthByEnv(string $envName): int
+    {
+        if ($envName === 'production') {
+            return self::SEEDER_DATA_LENGTH;
+        } else if ($envName === 'testing') {
+            // testの時
+            return self::SEEDER_DATA_LENGTH;
+        } else {
+            // localやstaging
+            // return self::SEEDER_DEVELOP_DATA_LENGTH;
+            return self::SEEDER_DATA_LENGTH;
+        }
     }
 }
