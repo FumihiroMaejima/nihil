8b3830cecf1fd6f301de95e02485b20ffbe3ac9c
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-04-24 21:40:21 +0900

    add colums to player & enemy models etc.

--- app/backend/app/Models/GameEnemies.php
+++ app/backend/app/Models/GameEnemies.php
@@ -11,6 +11,30 @@ class GameEnemies extends Model
     use HasFactory;
     use SoftDeletes;
 
+    // カラム一覧
+    public const ID = 'id';
+    public const NAME = 'name';
+    public const LEVEL = 'level';
+    public const HP = 'hp';
+    public const MP = 'mp';
+    public const OFFENCE = 'offence';
+    public const DEFENSE = 'defense';
+    public const SPEED = 'speed';
+    public const MAGIC = 'magic';
+    public const OFFENCE_EQUIPMENT_ID = 'offence_equipment_id';
+    public const DEFENSE_EQUIPMENT_ID = 'defense_equipment_id';
+    public const ABILITY1_ID = 'ability1_id';
+    public const ABILITY2_ID = 'ability2_id';
+    public const ABILITY3_ID = 'ability3_id';
+    public const ITEM = 'item';
+    public const EVENT_ONLY_FLG = 'event_only_flg';
+    public const ENCOUNT_AREA_ID = 'encount_area_id';
+    public const IMAGE_NAME = 'image_name';
+    public const IMAGE_URL = 'image_url';
+    public const CRREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
+    public const DELETED_AT = 'deleted_at';
+
     //テーブル名指定
     protected $table = 'game_enemies';
 
--- app/backend/app/Models/GameItem.php
+++ app/backend/app/Models/GameItem.php
@@ -11,6 +11,20 @@ class GameItem extends Model
     use HasFactory;
     use SoftDeletes;
 
+    // カラム一覧
+    public const ID = 'id';
+    public const NAME = 'name';
+    public const MESSAGE = 'message';
+    public const TARGET_COLUMN1 = 'target_column1';
+    public const TARGET_EFFECT1 = 'target_effect1';
+    public const TARGET_COLUMN2 = 'target_column2';
+    public const TARGET_EFFECT2 = 'target_effect2';
+    public const IMAGE_NAME = 'image_name';
+    public const IMAGE_URL = 'image_url';
+    public const CRREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
+    public const DELETED_AT = 'deleted_at';
+
     //テーブル名指定
     protected $table = 'game_item';
 
